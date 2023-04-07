771da0305f7bb43b73b6909f9e60bdb160dbddea
Saif Raza <72191211+razasaif@users.noreply.github.com>
2023-04-07 18:51:21 +0530

    Add files via upload

---
+++ NewsWebApp/index.js
@@ -0,0 +1,41 @@
+console.log('Welcome to London Times.');
+
+let source = 'bbc-news';
+let apiKey = 'fa0f2e6d6ad649b1968668a290ddabb9';
+
+let newsAccordion = document.getElementById('newsAccordion');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsText = "";
+        articles.forEach(function (element, index) {
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                <b>Breaking News ${index + 1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsText += news;
+        });
+        newsAccordion.innerHTML = newsText;
+    }
+    else {
+        console.log('Some error occured');
+    }
+}
+xhr.send();
+console.log('end');
